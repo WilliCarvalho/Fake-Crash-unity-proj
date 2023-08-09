bd832a5986850b126297cb957beb4936ff638ea2
Willian Carvalho <50246022+WilliCarvalho@users.noreply.github.com>
2023-08-09 19:04:12 -0300

    Player move forward related to the camera

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -60,9 +60,29 @@ public class Player : MonoBehaviour
 
     private void MovePlayer()
     {
-        transform.Translate(currentMovement * velocity * Time.deltaTime);
+        Vector3 cameraRelativeMovement = ConverToCameraSpace(currentMovement);
+        transform.Translate(cameraRelativeMovement * velocity * Time.deltaTime);        
+    }
 
-        
+    private Vector3 ConverToCameraSpace(Vector3 vectorToRotate)
+    {
+        float currentYValue = vectorToRotate.y;
+
+        Vector3 cameraForward = Camera.main.transform.forward;
+        Vector3 cameraRight = Camera.main.transform.right;
+
+        cameraForward.y = 0; 
+        cameraRight.y = 0;
+
+        cameraForward = cameraForward.normalized;
+        cameraRight = cameraRight.normalized;
+
+        Vector3 cameraForwardZproduct = vectorToRotate.z * cameraForward;
+        Vector3 cameraRightXProduct = vectorToRotate.x * cameraRight;
+
+        Vector3 vectorRotatedToCameraSpace = cameraForwardZproduct + cameraRightXProduct;
+        vectorRotatedToCameraSpace.y = currentYValue;
+        return vectorRotatedToCameraSpace;
     }
 
     private void OnCollisionEnter(Collision collision)
